a48967f68b2349e9754e9f1dbac44decdc6009e3
Abhishek <ak6726084@gmail.com>
2022-11-21 02:25:34 +0530

    create course and session year model

--- student_management_system/student_management/admin.py
+++ student_management_system/student_management/admin.py
@@ -5,3 +5,5 @@ from django.contrib.auth.admin import UserAdmin
 class UserModel(UserAdmin):
     list_display = ['username','user_type']
 admin.site.register(CustomUser,UserModel)
+admin.site.register(Course)
+admin.site.register(Session_Year)
